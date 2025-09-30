5e19a6c44a814788beae3270516e803ae6ba0102
jitendra singh thakur <jitendrasinghthakur@jitendras-MacBook-Air.local>
2025-09-30 12:52:58 +0530

    text data modified for home

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Team! Nice to meet you\n'
+  const msg = 'Hello Nice OWL Team! Good to meet you\n'
   res.end(msg);
 });
 
